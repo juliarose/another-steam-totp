a4a754f1ad266239897d91735a5e2ea734bf8742
juliarose <julialunarose@gmail.com>
2024-12-25 07:43:19 -0500

    Added  implementation for .

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.3.5 (2024-12-25)
+
+### Changed
+- Added `std::error::Error` implementation for `Error`.
+
 ## 0.3.4 (2024-12-25)
 
 ### Changed
--- src/error.rs
+++ src/error.rs
@@ -17,6 +17,8 @@ pub enum Error {
     IO(std::io::Error),
 }
 
+impl std::error::Error for Error {}
+
 impl From<std::io::Error> for Error {
     fn from(e: std::io::Error) -> Self {
         Self::IO(e)
